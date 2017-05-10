e59959eddd8075b24c0566038ae4d86e1a1c1478
blusoul <blusoul@users.noreply.github.com>
2017-05-10 18:11:03 +0800

    区划代码第一次读取成功

--- official/index-2015.js
+++ official/index-2015.js
@@ -2,6 +2,8 @@ const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const iconv = require('iconv-lite');
+const async = require('async');
+const _ = require('lodash');
 
 const entryUrl = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2015/index.html';
 const writeJSON = fs.createWriteStream(__dirname + '/data/list-2015.json');
@@ -26,18 +28,20 @@ const parseHtml = {
 
     linkList.map((index, item) => {
       const $item = $(item);
+      if (index < 3) {
+        provinceArr.push({
+          url: combineLink({
+            originUrl: entryUrl,
+            spliceUrl: $item.attr('href')
+          }),
+          name: $item.text().trim()
+        });
+      }
 
-      provinceArr.push({
-        url: combineLink({
-          originUrl: entryUrl,
-          spliceUrl: $item.attr('href')
-        }),
-        name: $item.text().trim()
-      });
     });
     return provinceArr;
   },
-  captureCity($, provinceUrl, provinceName) {
+  captureCity($) {
     const cityList = $('.citytr');
     let provinceCode = 0;
     let cityUrlArr = [];
@@ -50,14 +54,15 @@ const parseHtml = {
 
       if (!provinceCode) {
         provinceCode = code.substr(0, 2) + '0000000000';
-        listJSON[provinceCode] = provinceName;
+        listJSON[provinceCode] = $.provinceName;
       }
 
       listJSON[code] = name;
       cityUrlArr.push(combineLink({
-        originUrl: provinceUrl,
+        originUrl: $.href,
         spliceUrl: url
       }));
+
     });
 
     return cityUrlArr;
@@ -75,11 +80,9 @@ const parseHtml = {
       listJSON[code] = name;
       if (url) {
         areaUrlArr.push(combineLink({
-          originUrl: cityUrl,
+          originUrl: $.href,
           spliceUrl: url
         }));
-      } else {
-        console.log(name + '-----' + code)
       }
     });
 
@@ -103,7 +106,9 @@ const parseHtml = {
     Promise.all(provinceArr.map(item => {
         const provinceName = item.name;
         const provinceUrl = item.url;
-        return captureUrl(provinceUrl);
+        return new Promise((resolve, reject) => {
+          captureUrl(provinceUrl, resolve, provinceName);
+        })
       }))
       .then(res => {
         let tempArr = [];
@@ -111,97 +116,55 @@ const parseHtml = {
           tempArr = tempArr.concat(this.captureCity(item, item.href))
         });
 
-        return tempArr;
-      })
-      .then(res => {
-        console.log(listJSON)
-        return Promise.all(res.map(cityUrl => {
-          console.log(i++)
-          return captureUrl(cityUrl);
-        }))
-      })
-      .then(res => {
-        let tempArr = [];
-        res.map(item => {
-          tempArr = tempArr.concat(this.captureCountry(item, item.href))
+        async.mapLimit(tempArr, 10, function (item, callback) {
+          let temp = [];
+          captureUrl(item, function ($) {
+            console.log(item)
+            temp = temp.concat(parseHtml.captureCountry($));
+            callback(null, temp);
+          });
+        }, function (err, result) {
+          let tempArr = [];
+          result.map(item => {
+            tempArr = tempArr.concat(item);
+          });
+
+          async.mapLimit(tempArr, 10, function (item, callback) {
+            let temp = [];
+            captureUrl(item, function ($) {
+              console.log(item)
+              temp = temp.concat(parseHtml.captureTown($));
+              callback(null, temp);
+            });
+          }, function (err, result) {
+            writeJSON.write(JSON.stringify(listJSON));
+            console.log('区划信息写入成功~')
+          });
         });
-        return tempArr;
-      }).then(res => {
-        console.log(res)
-      }).catch(err => {
-        console.log(err)
       });
-
-
-    // Promise.all(provinceArr.map(item => {
-
-    //   const provinceName = item.name;
-    //   const provinceUrl = item.url;
-    //   return captureUrl(provinceUrl).then($ => {
-
-    //     const cityUrlArr = this.captureCity({
-    //       $,
-    //       provinceName,
-    //       provinceUrl
-    //     });
-
-    //     return Promise.all(cityUrlArr.map(cityUrl => {
-
-    //       return captureUrl(cityUrl).then(res => {
-
-    //         const countryUrlArr = this.captureCountry(res, cityUrl);
-
-    //         return Promise.all(countryUrlArr.map(countryUrl => {
-
-    //           return captureUrl(countryUrl).then(res => {
-    //             this.captureTown(res);
-    //           })
-
-    //         })).then(res => {
-    //           console.log(res);
-    //         })
-    //       });
-    //     }));
-    //   });
-    // })).then(res => {
-    //   console.log(222)
-    //   writeJSON.write(JSON.stringify(listJSON));
-    // }).catch(err => {
-    //   console.log(err)
-    // });
   }
-
 };
 
-setTimeout(function () {
-  writeJSON.write(JSON.stringify(listJSON));
-}, 60000)
-
-function captureUrl(url) {
-  return new Promise((resolve, reject) => {
-    request.get({
-      url: url,
-      encoding: null //让body 直接是buffer
-    }, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        const convertedHtml = iconv.decode(body, 'gb2312');
-        const $ = cheerio.load(convertedHtml, {
-          decodeEntities: false
-        });
-        $.href = url;
-        resolve($);
-      }
-      if (error) {
-        console.log(url)
-      }
-    });
+function captureUrl(url, fn, provinceName) {
+  request.get({
+    url: url,
+    encoding: null //让body 直接是buffer
+  }, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const convertedHtml = iconv.decode(body, 'gb2312');
+      const $ = cheerio.load(convertedHtml, {
+        decodeEntities: false
+      });
+      $.href = url;
+      $.provinceName = provinceName;
+      fn($);
+    }
+    if (error) {
+      console.log(url)
+    }
   });
 }
 
-// 获取博雅地名网数据
-captureUrl(entryUrl).then(res => {
-  // console.log(res)
+captureUrl(entryUrl, function (res) {
   parseHtml.deal(res, entryUrl);
-}).catch(res => {
-
 });
